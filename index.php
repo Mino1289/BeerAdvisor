413531e4998e54fd287a46ed758a9e7880f1353b
Mino1289 <kretzvictor@orange.fr>
2022-09-28 18:00:46 +0200

    Add first page + style and header

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Beer Advisor</title>
 </head>
 <body>
     <?php
-        echo "Vive la bière !";
+        include 'header.php';
     ?>
+    <h1 id="title">BEER ADVISOR</h1>
+    <input id="input" type="text" placeholder="Rechercher une bière">
 
 </body>
 </html>
