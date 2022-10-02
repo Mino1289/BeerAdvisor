b93bfaaaa5b8364d04ba5a17bf4bb3ec592d2654
Glenunciter <pourcinematteo@gmail.com>
2022-10-02 19:27:32 +0200

    no advancement :|

--- register.php
+++ register.php
@@ -4,21 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign-in</title>
+    <title>Register</title>
     <link rel="stylesheet" href="./css/style.css">
     <link rel="icon" href="./img/logo.ico" type="image/x-icon">
-    <?php include 'database.php' ; ?>
+
     
 </head>
 <body>
-<form name="sign" method="post" action="signin.php">
+<div id="form">
+<form name="sign" method="post" action="register.php">
    
-    <input  name="name" type="text" maxlength=20/>
-    <input  name="firstname" type="text" maxlength=20/>
-    <input  name="username" type="text" maxlength=20/>
+    <input  name='name' type="text" maxlength=20/>
+    <input  required name="firstname" type="text" maxlength=20/>
+    <input  required name="username" type="text" maxlength=20/>
     <input  required  name="mail" type="email" placeholder="email@domain " maxlength=60/>
     <input  required  name="password"  type="password" maxlength=40/>
-    <input  type="file" />
+    <input  name="profile_picture" type="file" />
     
     <select name="rank">
         <option value="novice">novice</option>
@@ -27,9 +28,13 @@
         <option value="expert">expert</option>
         <option value="professionnal">professionnal</option>
         <option value="alcoholic">alcoholic</option>
-    </select>
+    </select> -->
 
-    <input type="submit" />
+    <input name="validate" type="submit" />
 </form>
+
+
+</div>
+
 </body>
 </html>
