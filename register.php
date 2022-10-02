a0a353c5b89e241e80b2b6686599c79cbc9aa223
Glenunciter <pourcinematteo@gmail.com>
2022-10-02 18:21:51 +0200

    oups

---
+++ register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign-in</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="icon" href="./img/logo.ico" type="image/x-icon">
+    <?php include 'database.php' ; ?>
+    
+</head>
+<body>
+<form name="sign" method="post" action="signin.php">
+   
+    <input  name="name" type="text" maxlength=20/>
+    <input  name="firstname" type="text" maxlength=20/>
+    <input  name="username" type="text" maxlength=20/>
+    <input  required  name="mail" type="email" placeholder="email@domain " maxlength=60/>
+    <input  required  name="password"  type="password" maxlength=40/>
+    <input  type="file" />
+    
+    <select name="rank">
+        <option value="novice">novice</option>
+        <option value="amateur">amateur</option>
+        <option value="intermediate">intermediate</option>
+        <option value="expert">expert</option>
+        <option value="professionnal">professionnal</option>
+        <option value="alcoholic">alcoholic</option>
+    </select>
+
+    <input type="submit" />
+</form>
+</body>
+</html>
