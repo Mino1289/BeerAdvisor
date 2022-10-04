f0e292e1e601c5cb4adde2d478b3a4db695a7373
Glenunciter <pourcinematteo@gmail.com>
2022-10-04 23:44:48 +0200

    pas avancer lol

--- register.php
+++ register.php
@@ -11,14 +11,69 @@
     
 </head>
 <body>
-<?php include 'header.php'?>
+<?php include 'header.php'; ?>
 
+<?php 
+    include 'database.php';
+    $nameErr = $firstnameErr = $usernameErr = $mailErr =" ";  // Useful to display an error if there is already the same data in the table 'user'
+    $name= $firstname = $username = $mail = $password = $profile_picture = $rank =" ";
+    global $db;
+    $Bool = FALSE;
+    
+    // Function which is able to  look for double data in the same field
+    function __ishere($item,$nameOfField){
+        
+        $Booll = FALSE;
+        $isSame=" ";
+        $sql="SELECT $nameOfField FROM user";
+        $result = $db->prepare($sql);
+        $result->execute();
+        $items = $result->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($items as $isSame){
+            if($items == $isSame){
+                $Booll = TRUE;
+            }
+        }
+        return $Booll;
+    }
+
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") { // clean the input of a user when he submits
+
+         $name = test_input($_POST["name"]);
+         if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) { // preg_match look for a specific pattern in the string 
+            $nameErr = "Only letters and white space allowed";
+         }
+         $firstname = test_input($_POST["firstname"]);
+         $username = test_input($_POST["username"]);
+         $mail = test_input($_POST["mail"]);
+         if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) { // filter_var filters a variable with a specific filter. In this case it's for email
+             $mailErr = "Invalid mail format";
+         }
+         // Pas oublier de crypter le mdp
+         $password = test_input($_POST["password"]);
+        }
+
+    //Mettre dans un autre .php car déjà utilsé dans d'autre .php come sur le dev de vic 
+    function test_input($data) {
+        $data = trim($data); // Remove whitespace and other predifined caracter from both sides of a string
+        $data = stripslashes($data);// Remove backslashes
+        $data = htmlspecialchars($data);// Convert predifined caracters
+        return $data;
+    }
+
+
+    
+
+?>
 
 <div id="form">
     <form name="sign" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> <!-- Useful to prevent from attackers who can exploit the code by injecting javascript code -->
 
         <label for='name'>Name </label>
-        <input   required id='name' name='name' type="text" maxlength=20/></br>
+        <input   required id='name' name='name' type="text" maxlength=20 /><span class="error"><?php echo $nameErr;?></span>
+        </br>
         <label for='firstname'>Firstname</label>
         <input   required id='firstname' name="firstname" type="text" maxlength=20/></br>
         <label for='username'>Username</label>
@@ -40,66 +95,10 @@
             <option value="alcoholic">alcoholic</option>
         </select> </br>
 
-        <input name="validate" type="submit" value="Continue" />
+        <input name="submit" type="submit" value="Submit" />
     </form>
 </div>
 
-<?php 
-    include 'database.php';
-    $nameErr = $firstnameErr = $usernameErr = $mailErr =" ";  // Useful to display an error if there is already the same data in the table 'user'
-    $name= $firstname = $username = $mail = $password = $profile_picture = $rank =" ";
-    global $db;
-    
-    
-    // Function which is able to  look for double data in the same field
-    function __ishere($item,$nameOfField){
-        
-        $Booll = FALSE;
-        $isSame=" ";
-        $sql="SELECT $nameOfField FROM user";
-        $result = $db->prepare($sql);
-        $result->execute();
-        $items = $result->fetchAll(PDO::FETCH_ASSOC);
 
-        foreach ($items as $isSame){
-            if($items == $isSame){
-                $Booll = TRUE;
-            }
-        }
-        return $Booll;
-    }
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") { // clean the input of a user when he submits
-        
-        $name = test_input($_POST["name"]);
-        if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) { // preg_match look for a pattern in the string and return true in this case
-            $nameErr = "Only letters and white space allowed";
-        }
-
-
-        $firstname = test_input($_POST["firstname"]);
-
-        $username = test_input($_POST["username"]);
-
-        $mail = test_input($_POST["mail"]);
-        if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) { // filter var filters a variable with a specific filter. In this case it's for email
-            $mailErr = "Invalid mail format";
-        }
-        // Pas oublier de crypter le mdp
-        $password = test_input($_POST["password"]);
-    }
-
-    
-    function test_input($data) {
-        $data = trim($data); // Remove whitespace and other predifined caracter from both sides of a string
-        $data = stripslashes($data);// Remove backslashes
-        $data = htmlspecialchars($data);// Convert predifined caracters
-        return $data;
-    }
-
-
-    
-
-?>
 </body>
 </html>
