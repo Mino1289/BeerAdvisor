3793b59307e435330b1cdcbda6171e8ec969b2d6
paulcmt <paulcleent@gmail.com>
2022-10-04 09:07:13 +0200

    CSS --> SCSS pour animation plus simple

--- index.php
+++ index.php
@@ -4,14 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/style.scss">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Beer Advisor</title>
 </head>
 <body>
+
     <?php
         include 'header.php';
     ?>
+
     <h1 id="title">BEER ADVISOR</h1>
     <div id="search">
         <div id=""><i class="fa fa-search logosearch"></i></div>
