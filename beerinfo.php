a99c2768aae0630d122a5995d858c15721ea6d60
Mino1289 <kretzvictor@orange.fr>
2022-10-15 17:17:42 +0200

    Like/Dislike a beer

--- beerinfo.php
+++ beerinfo.php
@@ -78,9 +78,21 @@
             echo "<li>Average Grade : ".$grade."/5</li>";
             echo "</ul>";
             echo "<a href='add_beer.php?id=".$this->ID_beer."'><button id='edit_beer'>Edit this beer</button></a>";
+
+            $sql = "SELECT * FROM beer_user WHERE ID_beer = $this->ID_beer AND ID_user = ".$_SESSION['ID_user'];
+            $result = $db->prepare($sql);
+            $result->execute();
+            $beer_user = $result->fetch();
+            if (empty($beer_user)) {
+                $act = "Like";
+            } else {
+                $act = "Dislike";
+            }
+
             if (isset($_SESSION['ID_user'])) {
                 echo "<a href='add_comment.php?id=".$this->ID_beer."'><button>Add a comment</button></a>";
-                echo "<a href='like.php?id=".$this->ID_beer."'><button>Like</button></a>";
+                echo "<form method='post'>
+                <button type='submit'>$act</button></form>";
             }
             echo "</div>";
             echo "<link rel='stylesheet' href='./css/beer_display.scss'>";
