360abfdb2873ed84cf7deddabee4ed41693c5d00
Mino1289 <kretzvictor@orange.fr>
2022-10-19 16:04:19 +0200

    userinfo

--- userinfo.php
+++ userinfo.php
@@ -32,15 +32,19 @@
 
             $beeruser = $query->fetchAll(PDO::FETCH_ASSOC);
 
-            echo "<div class='user'>";
-            echo "<h1>".$this->username."</h1>";
-            echo '<img width="20em" heigth="20em" src="data:image/png;base64,'. base64_encode($this->profile_picture) . '" />';
-            echo "<p>".$this->name."</p>";
-            echo "<p>".$this->firstname."</p>";
-            echo "<p>".$this->mail."</p>";
-            echo "<p>".$this->rank."</p>";
+            
+            echo "<h1 id='username'>What about ".$this->username." ?</h1>";
+            echo "<div id='black_box'>";
+            echo "<img src='".$this->profile_picture."' alt='profile_picture'>";
+            echo "<ul id='information_user'>";
+            echo "<li>Name : ".$this->name."</li>";
+            echo "<li>Firstname : ".$this->firstname."</li>";
+            echo "<li>Mail : ".$this->mail."</li>";
+            echo "<li>Rank : ".$this->rank."</li>";
+            echo "</ul>";
+            echo "</div>";
+            
 
-            $follow;
             if (isset($_SESSION["ID_user"]) && $_SESSION["ID_user"] != $this->ID_user) {
                 $ID_user = $_SESSION["ID_user"];
 
@@ -58,25 +62,30 @@
                 <div id='follow_button'><button type='submit'>$act</button></div></form>";
             }
             
-            echo "</div>";
-            if (!empty($follow) || $_SESSION["ID_user"] == $this->ID_user) {
-                echo "<div class='beeruser'>";
-                echo "<h1>Bières</h1>";
-                foreach ($beeruser as $beer) {
-                    $sql = "SELECT * FROM beer INNER JOIN color ON beer.ID_color = color.ID_color 
-                            INNER JOIN taste ON beer.ID_taste = taste.ID_taste 
-                            INNER JOIN category ON beer.ID_category = category.ID_category 
-                            WHERE ID_beer = " .$beer["ID_beer"];
-                            
-                    $query = $db->prepare($sql);
-                    $query->execute();
+            echo "<div id='beeruser'>";
+            echo "<h1>Beers informations</h1>";
+            foreach ($beeruser as $beer) {
+                $sql = "SELECT * FROM beer INNER JOIN color ON beer.ID_color = color.ID_color 
+                        INNER JOIN taste ON beer.ID_taste = taste.ID_taste 
+                        INNER JOIN category ON beer.ID_category = category.ID_category 
+                        WHERE ID_beer = " .$beer["ID_beer"];
+                        
+                $query = $db->prepare($sql);
+                $query->execute();
 
-                    $beer = $query->fetch(PDO::FETCH_ASSOC);
-                    $beer = new Beer($beer['ID_beer'],$beer['name'],$beer['location'],$beer['color_name'],$beer['strength'],$beer['taste_name'],$beer['brewery'], $beer['category_name']);
-                    $beer->display_box();
-                }
-                echo "</div>";
+                $beer = $query->fetch(PDO::FETCH_ASSOC);
+                $beer = new Beer($beer['ID_beer'],$beer['name'],$beer['location'],$beer['color_name'],$beer['strength'],$beer['taste_name'],$beer['brewery'], $beer['category_name']);
+                $beer->display_box();
+                echo "<div class='border_separation'></div>";
             }
+            
+            echo "<div class='border_separation_undo'></div>";
+        
+            echo "</div>";
+            
+            echo "<link rel='stylesheet' href='./css/user.scss'>";
+
         }
+
     }
 ?>
