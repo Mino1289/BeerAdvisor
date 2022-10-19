086e65a4cfb415f16c08e55cbd22a1db4427de5d
Mino1289 <kretzvictor@orange.fr>
2022-10-19 15:54:46 +0200

    disconnect btn only if user logged in

--- header.php
+++ header.php
@@ -10,7 +10,11 @@
         <a href="./add_beer.php">Add a beer</a>
         <a href="./login.php">Sign in</a>
         <a href="./register.php">Sign up</a>
-        <a href="./disconnect.php">Disconnect</a>
+        <?php 
+            if (isset($_SESSION["ID_user"])) {
+                echo '<a href="./disconnect.php">Disconnect</a>';
+            }
+        ?>
     </div>
     <script>
         function changeState(x) {
